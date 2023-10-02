ae97d4c00a1b33342d9548efaad5c510a5dde26a
unknown <wendeltpds>
2023-10-02 02:11:19 -0300

    mudando sobre

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -3,7 +3,9 @@ import './Sobre.css'
 import {FaReact  , FaCss3Alt} from 'react-icons/fa'
 import {BiLogoJavascript , BiLogoTypescript} from 'react-icons/bi'
 import {AiFillHtml5} from 'react-icons/ai'
+import {SiNextdotjs} from 'react-icons/si'
 import { motion } from 'framer-motion'
+
 
 
 
@@ -21,7 +23,7 @@ const Sobre = () => {
             <h1 className='nome-sobremim' >Sobre Mim</h1>
             <p className='texto-sobre' >
                 meu nome e Wendel Tadeu e sou apaixonado por tecnologia e decidi trilhar o camiho da programação ,
-                atualmente estou  focando nas linguagens  voltadas para o front-end , HTML , CSS ,Javascript , Typescript e react.js  mas  estou estudando node.js.
+                atualmente estou  focando nas linguagens  voltadas para o front-end , HTML , CSS ,Javascript , Typescript , react.js e next.js  mas  estou estudando node.js e pretendo estudar java.
             </p>
 
             <h1 className='linguagens-sobre' >linguagens</h1>
@@ -31,6 +33,8 @@ const Sobre = () => {
                 <FaCss3Alt  className='logo-react-sobre' />
                 <AiFillHtml5  className='logo-react-sobre' />
                 <BiLogoTypescript  className='logo-react-sobre' />
+                <SiNextdotjs  className='logo-react-sobre' />
+
             </div>
         </motion.div>
         )
